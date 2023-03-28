8af9f34bd1f4c324abd2821cc0bd05cf0e757571
Carl Csaposs <carl.csaposs@canonical.com>
2023-03-28 18:59:04 +0000

    When both charm & package depend on tenacity, same version of tenacity is installed
    unit-mysql-0: 18:58:29 WARNING unit.mysql/0.install 8.0.1
    unit-mysql-0: 18:58:29 WARNING unit.mysql/0.install local version: 8.0.1

--- machine/build.sh
+++ machine/build.sh
@@ -1,5 +1,7 @@
 # cd to script directory
 cd "$(dirname "${BASH_SOURCE[0]}")"
+# todo: get LXD container name dynmaically
+# todo: don't fail rm if file does not exist
 sudo rm /var/snap/lxd/common/lxd/containers/charmcraft_charmcraft-mysql-2325476-0-0-amd64/rootfs/root/parts/charm/build/charmcraft-dependencies-hash.txt
 cp -r ../common/ .
 charmcraft pack
