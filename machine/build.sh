2724e573b12dc9bea128d23252780231bb75fafc
Carl Csaposs <carl.csaposs@canonical.com>
2023-03-28 18:19:58 +0000

    Force charmcraft to update dependencies without clearing cache

--- machine/build.sh
+++ machine/build.sh
@@ -1,5 +1,6 @@
 # cd to script directory
 cd "$(dirname "${BASH_SOURCE[0]}")"
+sudo rm /var/snap/lxd/common/lxd/containers/charmcraft_charmcraft-mysql-2325476-0-0-amd64/rootfs/root/parts/charm/build/charmcraft-dependencies-hash.txt
 cp -r ../common/ .
 charmcraft pack
 rm -r ./common/
